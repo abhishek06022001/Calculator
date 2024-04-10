a346f52b9f728c870e19085f4fa2b1e6c05d9955
Abhishek <satheabhishek1@gmail.com>
2024-04-11 00:04:29 +0530

    fix some issues and completed it

--- script.js
+++ script.js
@@ -20,33 +20,76 @@ document.addEventListener('DOMContentLoaded',function(){
     var buttons = document.querySelectorAll('.button');
     var operator = document.querySelectorAll('.operator');
     var div = document.querySelectorAll('.display');
+    var ans = document.querySelectorAll('.ans');
     var action = "";
     var flag = 0 ;
+    var secondnum = "";
+    var isnumcomplete = 0 ;
+    var number1 = "";
+    // operator dabane k baad
     for(let oper of operator)
-    {   
+    {       
             oper.addEventListener('click',function()
-            {
+            {   
+                if(flag == 1)
+                {
+                    let num1 = Number(number1);
+                    let num2 = Number(secondnum);
+                    div[0].textContent = operate(num1,num2,action);
+                    number1 = div[0].textContent;
+                    secondnum="";
+                }
                  action = oper.textContent;
+                //  isnumcomplete = 1;
                 flag = 1;
             })
 
     }
     var dispvalue="";
+    ans[0].addEventListener('click',function()
+    {
+        // var div = document.getElementsByClassName('display');
+        // let numberone = div[0].textContent;
+        let num1 = Number(number1);
+        let num2 = Number(secondnum);
+        div[0].textContent = operate(num1,num2,action);
+    })
     for(let button of buttons)
     {
        button.addEventListener('click',function()
        {        
-            if(flag==1)
-            {   
+            if(flag==1 && isnumcomplete==1 )
+            {    
                 let num1 = Number(div[0].textContent);
-                let num2 = Number(button.textContent);
+                secondnum += button.textContent; 
+                let num2 = Number(secondnum);
                 // alert(operate(num1,num2,action));
                 div[0].textContent=operate(num1,num2,action);
                 flag = 0;
-            }else{
-                let displayValue = button.textContent;
-                append(displayValue);
+            }else if(flag == 1 && isnumcomplete != 1)
+            {
+                // second num
+                // alert("yo");
+                var div = document.getElementsByClassName('display');
+                div[0].textContent = "";
+                dispvalue = "";
         
+                secondnum += button.textContent; 
+                let displayValue = secondnum;
+                append(displayValue);
+                // alert(number1);
+
+
+            }else{
+                // only one num
+                // alert("2");
+                append(button.textContent);
+                let div = document.getElementsByClassName('display');
+                 number1 = div[0].textContent;
+                //  alert("hey");
+                
+               
+                      
             }
        })
     }
@@ -86,6 +129,10 @@ document.addEventListener('DOMContentLoaded',function(){
         var div = document.getElementsByClassName('display');
         div[0].textContent = "";
         dispvalue = "";
+        number1 = "";
+        secondnum="";
+        flag= 0;
+        isnumcomplete = "";
 
     })
    
